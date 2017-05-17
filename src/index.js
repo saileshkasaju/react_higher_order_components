250af842c047e33850b4aaabb750b4d478b4870b
saileshkasaju <leosailesh@gmail.com>
2017-05-18 02:44:49 +0545

    A higher order component typically always has the same structure. It takes in a BaseCompenent and it returns a component, in this case, it's a functional stateless component. Then it renders out the BaseCompenent and it spreads -> ... the props in. e.g hoc Now, if we create another User and we pass our first User to our higher order component -> hoc, it's not going to modify it in any way, but we will have a valid React component. I just have to add the name. You see, the prop signature is identical to User, and it renders exactly the same.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
 
+const hoc = (BaseComponent) => (props) =>
+    <BaseComponent {...props}/>;
+
 const User = ({ name }) =>
-<div className="User">{ name }</div>;
+    <div className="User">{ name }</div>;
+
+const User2 = hoc(User);
 
 const App = () =>
-<div>
-    <User name="Tim" />
-</div>;
+    <div>
+        <User name="Tim" />
+        <User2 name="Joe" />
+    </div>;
 
 ReactDOM.render(
   <App />,
