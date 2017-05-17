0c313c7f4f1bf0d0fe39b870d429b08a901a1db4
saileshkasaju <leosailesh@gmail.com>
2017-05-18 03:04:58 +0545

    nother reason you might want to create a higher order component is to tap into a lifecycle hook. In this case, I need to use a class component and we have to do the same thing where we return the BaseCompenent and spread the props in. Perhaps we want to create a component that never updates.
    We could tap into the shouldComponentUpdate() method, and we can return
    false. We don't need a configuration step for this phase. We can call
    this neverRender.
    Now, User3 will never render.
    
    Now, if Steve were bound to some data and it changed the update would
    not be reflected.

--- src/index.js
+++ src/index.js
@@ -7,18 +7,28 @@ const hoc = (BaseComponent) => (props) =>
 
 const overrideProps = (overrideProps) => (BaseComponent) => (props) =>
     <BaseComponent {...props} {...overrideProps} />;
+const neverRender = (overrideProps) => (BaseComponent) =>
+    class extends React.Component {
+        shouldComponentUpdate() {
+            return false;
+        }
+        render() {
+            return <BaseComponent {...this.props}/>
+        }
+    };
 
 const User = ({ name }) =>
     <div className="User">{ name }</div>;
 const User1 = hoc(User);
 const alwaysBob = overrideProps({ name: 'Bob' });
 const User2 = alwaysBob(User);
-
+const User3 = neverRender(User);
 const App = () =>
     <div>
         <User name="Tim" />
         <User1 name="Joe" />
         <User2 name="Joe" />
+        <User3 name="Steve" />
     </div>;
 
 ReactDOM.render(
