6af28523d8dfa97deaab5392dc0618af669affdd
saileshkasaju <leosailesh@gmail.com>
2017-05-18 02:58:31 +0545

    Now if we want our higher order component to actually do something, typically you add a configuration step. Here we'll take in some props we'd like to override and then we can spread those onto the component as well. Now, we have to use our configuration phase and pass in any prop that we'd like to override. Now the name will always be 'Bob', regardless of what I change the name in the JSX. You could save the configuration portion of the higher order component call into a variable. We can call this one alwaysBob, sometimes that's nice for readability. Then we can name our higher order component properly, as well.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 import "./index.css";
 
 const hoc = (BaseComponent) => (props) =>
-    <BaseComponent {...props}/>;
+    <BaseComponent {...props} />;
+
+const overrideProps = (overrideProps) => (BaseComponent) => (props) =>
+    <BaseComponent {...props} {...overrideProps} />;
 
 const User = ({ name }) =>
     <div className="User">{ name }</div>;
-
-const User2 = hoc(User);
+const User1 = hoc(User);
+const alwaysBob = overrideProps({ name: 'Bob' });
+const User2 = alwaysBob(User);
 
 const App = () =>
     <div>
         <User name="Tim" />
+        <User1 name="Joe" />
         <User2 name="Joe" />
     </div>;
 
